9403b0569cc14ae4f7b75902ab666a025eb77247
JuanRudolph <jrudolph@mail.austral.edu.ar>
2026-08-19 15:37:57 -0300

    feat(back): add FirebaseAuthGuard and protect users endpoints

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { FirebaseAuthGuard } from '../auth/firebase-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
+@UseGuards(FirebaseAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
