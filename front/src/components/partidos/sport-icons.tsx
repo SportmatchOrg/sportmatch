910391ed1c648a8b6864e743c2d6207f69ff08b3
IgnacioPacheco61 <ipacheco@mail.austral.edu.ar>
2026-08-27 13:55:22 -0300

    feat(front): agregar los iconos de deportes extraidos de Figma
    lucide no tiene pelota de futbol, basquet, tenis ni paleta de padel. Se exportaron del diseno y heredan el color con currentColor.

---
+++ front/src/components/partidos/sport-icons.tsx
@@ -0,0 +1,60 @@
+import type { SVGProps } from 'react';
+
+type SportIconProps = SVGProps<SVGSVGElement>;
+
+export function FutbolIcon(props: SportIconProps) {
+  return (
+    <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M11 20.1667C16.0626 20.1667 20.1667 16.0626 20.1667 11C20.1667 5.93739 16.0626 1.83333 11 1.83333C5.93739 1.83333 1.83333 5.93739 1.83333 11C1.83333 16.0626 5.93739 20.1667 11 20.1667Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M6.325 9.44167C8.15833 7.33333 10.9083 6.14167 10.9083 6.14167C13.6583 7.33333 15.4917 9.53333 15.4917 9.44167C15.4917 12.7417 13.75 14.85 13.75 14.85C10.9083 15.4917 8.06667 14.85 8.06667 14.85C6.325 12.7417 6.325 9.44167 6.325 9.44167Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M10.9083 6.14167V1.83333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M15.4917 9.53333C15.4917 9.53333 18.2417 8.25 19.6167 8.06667" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M13.75 14.9417C13.75 14.9417 15.4917 17.4167 16.4083 18.3333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M8.06667 14.9417C8.06667 14.9417 6.325 17.4167 5.5 18.3333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M2.38333 7.975C3.66667 8.25 6.41667 9.53333 6.41667 9.53333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function BasquetIcon(props: SportIconProps) {
+  return (
+    <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M11 20.1667C16.0626 20.1667 20.1667 16.0626 20.1667 11C20.1667 5.93739 16.0626 1.83333 11 1.83333C5.93739 1.83333 1.83333 5.93739 1.83333 11C1.83333 16.0626 5.93739 20.1667 11 20.1667Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M1.925 12.2833C3.32796 12.4628 4.75339 12.3185 6.09188 11.8614C7.43037 11.4043 8.64635 10.6466 9.64647 9.64647C10.6466 8.64635 11.4043 7.43037 11.8614 6.09188C12.3185 4.75339 12.4628 3.32796 12.2833 1.925" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M4.58333 4.49167L17.4167 17.5083" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M20.075 9.71667C18.672 9.53719 17.2466 9.68153 15.9081 10.1386C14.5696 10.5957 13.3537 11.3534 12.3535 12.3535C11.3534 13.3537 10.5957 14.5696 10.1386 15.9081C9.68153 17.2466 9.53719 18.672 9.71667 20.075" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function TenisIcon(props: SportIconProps) {
+  return (
+    <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M1.83333 11C6.875 11 11 6.875 11 1.83333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M11 20.1667C16.0626 20.1667 20.1667 16.0626 20.1667 11C20.1667 5.93739 16.0626 1.83333 11 1.83333C5.93739 1.83333 1.83333 5.93739 1.83333 11C1.83333 16.0626 5.93739 20.1667 11 20.1667Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M20.1667 11C15.125 11 11 15.125 11 20.1667" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function PadelIcon(props: SportIconProps) {
+  return (
+    <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M18.7917 3.20833C16.5917 1.00833 12.5583 1.55833 9.80833 4.30833C8.70794 5.39538 7.91694 6.7559 7.51667 8.25C7.05833 10.175 7.425 12.0083 8.70833 13.2917C9.99167 14.575 11.825 14.9417 13.75 14.4833C15.2441 14.0831 16.6046 13.2921 17.6917 12.1917C20.4417 9.44167 20.9917 5.40833 18.7917 3.20833Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M7.51667 8.25L5.5 16.5L13.75 14.4833" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M1.83333 20.1667L5.5 16.5" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M18.3333 20.1667C19.3459 20.1667 20.1667 19.3459 20.1667 18.3333C20.1667 17.3208 19.3459 16.5 18.3333 16.5C17.3208 16.5 16.5 17.3208 16.5 18.3333C16.5 19.3459 17.3208 20.1667 18.3333 20.1667Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function RunningIcon(props: SportIconProps) {
+  return (
+    <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M3.66667 14.6667V12.485C3.66667 10.5417 2.7225 9.625 2.75 7.33333C2.7775 4.84 4.11583 1.83333 6.875 1.83333C8.58917 1.83333 9.16667 3.48333 9.16667 5.04167C9.16667 7.8925 7.33333 10.23 7.33333 12.9983V14.6667C7.33333 15.1529 7.14018 15.6192 6.79636 15.963C6.45255 16.3068 5.98623 16.5 5.5 16.5C5.01377 16.5 4.54745 16.3068 4.20364 15.963C3.85982 15.6192 3.66667 15.1529 3.66667 14.6667Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M18.3333 18.3333V16.1517C18.3333 14.2083 19.2775 13.2917 19.25 11C19.2225 8.50667 17.8842 5.5 15.125 5.5C13.4108 5.5 12.8333 7.15 12.8333 8.70833C12.8333 11.5592 14.6667 13.8967 14.6667 16.665V18.3333C14.6667 18.8196 14.8598 19.2859 15.2036 19.6297C15.5475 19.9735 16.0138 20.1667 16.5 20.1667C16.9862 20.1667 17.4525 19.9735 17.7964 19.6297C18.1402 19.2859 18.3333 18.8196 18.3333 18.3333Z" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M14.6667 15.5833H18.3333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M3.66667 11.9167H7.33333" stroke="currentColor" strokeWidth="1.83333" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
