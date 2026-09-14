728df3550b3dae3222eada438e872050f8e5fa20
JuanRudolph <jrudolph@mail.austral.edu.ar>
2026-09-14 09:33:27 -0300

    refactor(back): resolve Argentina week boundaries with the IANA time zone

--- back/src/utils/time/week-streak.ts
+++ back/src/utils/time/week-streak.ts
@@ -1,10 +1,17 @@
-const ARGENTINA_UTC_OFFSET_MS = 3 * 60 * 60 * 1000;
+const ARGENTINA_TIME_ZONE = 'America/Argentina/Buenos_Aires';
 const MS_PER_DAY = 24 * 60 * 60 * 1000;
 const EPOCH_TO_MONDAY_OFFSET_DAYS = 3;
 
+const argentinaDate = new Intl.DateTimeFormat('en-CA', {
+  timeZone: ARGENTINA_TIME_ZONE,
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
+
 const toWeekIndex = (date: Date): number => {
-  const argentinaTime = date.getTime() - ARGENTINA_UTC_OFFSET_MS;
-  const daysSinceEpoch = Math.floor(argentinaTime / MS_PER_DAY);
+  const [year, month, day] = argentinaDate.format(date).split('-').map(Number);
+  const daysSinceEpoch = Date.UTC(year, month - 1, day) / MS_PER_DAY;
 
   return Math.floor((daysSinceEpoch + EPOCH_TO_MONDAY_OFFSET_DAYS) / 7);
 };
