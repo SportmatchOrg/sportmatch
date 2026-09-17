247e65d5912ff69cb11b1bde92abae6d17104c24
JuanRudolph <jrudolph@mail.austral.edu.ar>
2026-09-17 18:49:19 -0300

    fix(back): provision the user row on authentication

--- back/src/deportes/deportes.module.ts
+++ back/src/deportes/deportes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { DeportesController } from './deportes.controller';
 import { DeportesRepository } from './deportes.repository';
 import { DeportesService } from './deportes.service';
 
 @Module({
+  imports: [UsersModule],
   controllers: [DeportesController],
   providers: [DeportesService, DeportesRepository],
   exports: [DeportesService],
